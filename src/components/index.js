d5c8698dd9dc6b6b82645b2f51c1cc2bd4954988
akshitak27 <akshitakhandelwal201@gmail.com>
2024-07-05 00:24:49 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { Article} from './article/Article';
+export { Brand} from './brand/Brand';
+export { CTA} from './cta/CTA';
+export { Feature} from './feature/Feature';
+export {Navbar} from './navbar/Navbar';
+
---
+++ src/containers/index.js
@@ -0,0 +1,9 @@
+export {  Footer } from './footer/Footer';
+export { Blog } from './blog/Blog';
+export{  Features } from './features/Features';
+export { Header} from './header/Header';
+export { Possibility} from './possibility/Possibility';
+export { WhatGPT3} from './whatGPT3/WhatGPT3'
+
+
+
